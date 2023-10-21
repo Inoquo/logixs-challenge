468a369e506ebf24aec4c5c1e52b1f2bd1811f9e
enriques2dam <enriques.2dam@gmail.com>
2023-10-21 20:01:49 +0200

    arreglos configuracion

--- api-gateway/src/main/java/com/logixs/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/logixs/gateway/ApiGatewayApplication.java
@@ -6,11 +6,10 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
-
 }
